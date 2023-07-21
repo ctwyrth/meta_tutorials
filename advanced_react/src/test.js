f8e3f4125351d79913a826be51250ceb7b2d1ee4
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-07-21 10:02:45 -0700

    54th, Finishing advanced react and misc.

--- advanced_react/src/test.js
+++ advanced_react/src/test.js
@@ -1,15 +1,32 @@
-let x = 15;
-let y = 10;
-let z = 9;
+// let x = 15;
+// let y = 10;
+// let z = 9;
 
-let result = (x < y) && (y < z);
+// let result = (x < y) && (y < z);
 
-console.log(result);
+// console.log(result);
 
-let q = [1, 2, 3];
-q[10] = 10;
+// let q = [1, 2, 3];
+// q[10] = 10;
 
-console.log(q.length);
+// console.log(q.length);
 
-q.length = 1;
-console.log(q);
+// q.length = 1;
+// console.log(q);
+
+// let x = [1, 2, 3, 4, 5];
+// let [, q, , r] = x;
+
+// console.log(q, r);
+
+const person = {
+  name: "Leonardo",
+};
+
+let animal = {
+  species: "snake",
+};
+
+person = animal;
+
+console.log(person.species);
