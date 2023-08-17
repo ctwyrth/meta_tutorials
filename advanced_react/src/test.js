bd29f50df2e8037e393247e9bb4161ee4080f76b
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-08-17 09:22:41 -0700

    UI/UX cont'd

--- advanced_react/src/test.js
+++ advanced_react/src/test.js
@@ -33,33 +33,36 @@
 // console.log(person.name);
 // console.log(person);
 
-const arr = [1, 2, 3];
+// const arr = [1, 2, 3];
 
-console.log(arr);
+// console.log(arr);
 
-arr[-1] = 4;
+// arr[-1] = 4;
 
-console.log(arr);
-console.log(arr.length);
+// console.log(arr);
+// console.log(arr.length);
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(i + " - " + arr[i]);
-}
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(i + " - " + arr[i]);
+// }
 
-console.log(arr[-1]);
-console.log(arr[3]);
+// console.log(arr[-1]);
+// console.log(arr[3]);
 
-arr[-2] = 5;
+// arr[-2] = 5;
 
-console.log(arr);
-console.log(arr.length);
+// console.log(arr);
+// console.log(arr.length);
 
-arr[4] = 6;
-arr[5] = 7;
+// arr[4] = 6;
+// arr[5] = 7;
 
-console.log(arr);
-console.log(arr.length);
+// console.log(arr);
+// console.log(arr.length);
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(i + " - " + arr[i]);
-}
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(i + " - " + arr[i]);
+// }
+
+const arr = new Set([1, 2, 3]);
+console.log(typeof arr);
